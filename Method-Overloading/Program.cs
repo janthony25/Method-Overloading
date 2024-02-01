bda347a753113a048618a7ee21f74edc516e07f1
janthony25 <junanthony.business@gmail.com>
2024-02-01 18:20:23 +1300

    Add project files.

---
+++ Method-Overloading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method overloading = methods share the same name, but different parameters
+            //                      name + parameters = signature
+            //                      methods must have a unique signature
+
+
+            double total;
+
+            total = Multiply(2.4, 4, 3);
+
+            Console.WriteLine(total);
+
+
+            Console.ReadKey();
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Multiply(double a, double b, double c)
+        {
+            return (a * b + c);
+        }
+    }
+}
